5c333be5e541fcb6b928d1d1a58484034d6dd1ca
gabrielgjsc <gjesussouzacesario@gmail.com>
2024-05-30 19:54:25 -0300

    modified:   login.html modified:   main.js modified:   styles/styles.css new file:   styles/styles_system.css modified:   system.html

--- main.js
+++ main.js
@@ -67,3 +67,26 @@ let textoAjustado;
 }
     telefone.value = textoAjustado;
 }
+
+//Seleciona os itens clicado
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+//Expandir o menu
+
+var btnExp = document.querySelector('#btn-expandir')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
